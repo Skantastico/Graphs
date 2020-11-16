4e7f4f1d342604110b43f400a410c77d87a45e72
Skantastico <Skantastico@gmail.com>
2020-11-16 11:13:13 -0500

    imported Queue, added get ancestor helper function, made first attempt at solving

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,42 @@
+# import Queue from util in graphs
+
+import sys
+sys.path.append("projects/graph")
+from util import Queue
+
+
+def get_ancestor(ancestors, child):
+    heirs = []
+    for heir in ancestors:
+        if heir[1] == child:
+            heirs.append(heir[0])
+    return heirs
+
 
 def earliest_ancestor(ancestors, starting_node):
-    ### test
+    # starting conditions
+    q = Queue()
+    q.enqueue([starting_node])
+    visited = set()
+    path_len = 1
+    oldest_parent = -1
+
+    # while loop to search
+    while q.size() > 0:
+        path = q.dequeue()
+        current_node = path[-1]
+
+        if current_node not in visited:
+            visited.add(current_node)
+
+        if len(path) >= path_len and current_node < oldest_parent or \
+        len(path) > path_len:
+            path_len = len(path)
+            oldest_parent = current_node
+
+        for parent in get_ancestor(ancestors, current_node):
+            path_copy = list(path)
+            path_copy.append(parent)
+            q.enqueue(path_copy)
+
+    return oldest_parent
